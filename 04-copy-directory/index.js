d6d5a5bb6f074ca5f2bcbc8d4b2c363d9df19d55
OksanaZhuravel <opuseva@mail.ru>
2022-11-07 17:44:21 +0400

    feat: add task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const fsPromises = fs.promises;
+const copy = fsPromises.copyFile;
+const path = require('path');
 
+(function copyDir() {
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) {
+      throw new Error('new error');
+    }
+    console.log('Copy folder');
+  });
+  fsPromises.readdir(path.join(__dirname, 'files')).then((files) => {
+    files.forEach((file) => {
+      let filePath = path.join(__dirname, 'files', file);
+      copy(filePath, path.join(__dirname, 'files-copy', file));
+      console.log(file);
+    });
+  });
+})();
